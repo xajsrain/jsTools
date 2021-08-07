7fba6a766a23da00055d78e434154f94943b8a32
raincool <1528067402@qq.com>
2021-08-07 22:01:52 +0800

    master push 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,6 @@
 const fse = require('fs-extra')
 const path = require('path')
 
-const OriginDir = path.resolve(__dirname, '../demo')
-const TargetDir = path.resolve(__dirname, '../lib/')
-const rReadPath = (file) => {
-  fse.readdir(file, (err, data) => {
-    if (!data) return
-    data.forEach(item => {
-      const pathname = path.join(file, item);
-      if (fse.statSync(path.join(pathname)).isDirectory()) {
-        rReadPath(pathname)
-      } else {
-        if (item.search(/.base.[js|css|png|jpg]/) > 0) {
-          fse.copy(pathname, path.join(TargetDir, item.replace('.base', ''))).then(res => {
-            console.log(`\x1B[36m /test/: \x1B[33m ${item} \x1B[36m => /lib/ OK! `);
-          })
-        }
-      }
-    })
-  })
-}
+console.log('hello world with master branch!');
 // rReadPath(OriginDir)
+console.log("i am raincool");
